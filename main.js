8a8661c17fdb5927dfe031743e7c8403f68e7e93
jd.elliott <jde.cherryview@gmail.com>
2024-12-01 20:37:15 +0000

    replaced the alerts with modals
    the alerts have been replaced with modals as they are more accessible
    
    the modals appearance does need to be fixed

--- main.js
+++ main.js
@@ -4,15 +4,41 @@ function validateForm() {
     let email = document.getElementById("email").value;
     // avoiding blank input
     if (name === "" || email === "") {
-        alert("Name and email are required");
+        //alert("Name and email are required");
+        modalBlankFields.style.display = "block";
+
         return false;
     }
     // validating email format using a simple regular expression
     let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
-        alert("Please enter a valid email address");
+        modalUnvalidEmail.style.display = "block";
         return false;
     }
     // if all validations pass
     return true;
+}
+
+var modalBlankFields = document.getElementById("modalBlankFields");
+var modalUnvalidEmail = document.getElementById("modalUnvalidEmail");
+
+// Get the <span> element that closes the modal
+var spanBkFd = document.getElementById("closeBkFd");
+var spanUvEm = document.getElementById("closeUvEm");
+
+// When the user clicks on <span> (x), close the modal
+spanBkFd.onclick = function () {
+    modalBlankFields.style.display = "none";
+}
+spanUvEm.onclick = function () {
+    modalUnvalidEmail.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modalBlankFields) {
+        modalBlankFields.style.display = "none";
     }
+    if (event.target == modalUnvalidEmail) {
+        modalUnvalidEmail.style.display = "none";
+    }
+} 
